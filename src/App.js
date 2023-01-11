a986cba8bc23a769568419eb02c030b5dbe4e694
muhammhafizh <hafizhsusanto9@gmail.com>
2023-01-11 15:06:49 +0700

    product details page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Congratulation from "./pages/Congratulation";
 import Details from "./pages/Details";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
+import Provider from "./helpers/hooks/useGlobalContext";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/categories/:idc" element={<Details />} />
-        <Route path="/categories/:idc/products/:idp" element={<Details />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/congratulation" element={<Congratulation />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Provider>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/categories/:idc" element={<Details />} />
+          <Route path="/categories/:idc/products/:idp" element={<Details />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/congratulation" element={<Congratulation />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
